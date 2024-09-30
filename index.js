76603d34317db154d53bc421e951491e89052f09
Rishi100304 <rishianand1018@gmail.com>
2024-09-30 20:35:33 +0530

    Project push

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 import bodyParser from "body-parser";
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 let posts = [];
-app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
 app.get("/", (req,res) => {
